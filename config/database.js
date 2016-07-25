70b327909eea5b325390da614d889f7a87d3268d
KorwinS <korwin.stevens@gmail.com>
2016-07-25 15:51:17 -0400

    changed db url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://rooty:rooterson@ec2-54-211-28-7.compute-1:27017/dummyDB',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
